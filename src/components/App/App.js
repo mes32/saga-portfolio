f6895ad462ce4548d7cd5ae2a0666f90bc3b36f5
mes32 <stockman.mike@gmail.com>
2019-02-01 17:18:06 -0600

    Add component ProjectPage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import ProjectPage from '../ProjectPage/ProjectPage.js';
 
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
+        <Router>
+          <Route exact path="/" component={ProjectPage} />
+        </Router>
         <p>Empty Page</p>
       </div>
     );
---
+++ src/components/ProjectPage/ProjectPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ProjectPage extends Component {
+    render() {
+        return (
+            <div>
+                <header className="App-header">
+                    <h1>Project Page</h1>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default ProjectPage

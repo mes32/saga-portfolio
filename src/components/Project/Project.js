7aab71f784d74a2214ab3a8b705804f9687f9a74
mes32 <stockman.mike@gmail.com>
2019-02-01 22:19:13 -0600

    Add conditional rendering to non-required project attributes.

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -3,20 +3,60 @@ import './Project.css';
 
 class Project extends Component {
 
-    
+    putDescription() {
+        if (this.props.project.description) {
+            return <p>{this.props.project.description}</p>;
+        }
+    }
 
+    // Return in image of the project if available
+    putThumbnail() {
+        if (this.props.project.thumbnail) {
+            return (
+                <img 
+                    src={this.props.project.thumbnail} 
+                    alt={this.props.project.desciption} 
+                    className="thumbnail" 
+                />
+            );
+        } else {
+            return <div className="thumbnailPlaceholder"></div>;
+        }
+    }
+
+    // Return a link to the project github repo if available
+    putGitHub() {
+        if (this.props.project.github) {
+            return <a href={this.props.project.github} target="_blank">GitHub</a>;
+        }
+    }
+
+    // Return a link to the project website if available
+    putWebsite() {
+        if (this.props.project.website) {
+            return <a href={this.props.project.website} target="_blank">Website</a>;
+        }
+    }
+
+    // Return the project tag if available
+    putTag() {
+        if (this.props.project.tag) {
+            return this.props.project.tag;
+        }
+    }
+
+    // Show this component on the page
     render() {
         const project = this.props.project;
         return (
             <div>
-                {/* <p>{JSON.stringify(project)}</p> */}
                 <h3>{project.name}</h3>
-                <p>{project.description}</p>
-                <img src={project.thumbnail} alt={project.desciption} className="thumbnail" />
+                {this.putDescription()}
+                {this.putThumbnail()}
                 <p>
-                    <a href={project.github}>GitHub</a> | 
-                    <a href={project.website}>Website</a> | 
-                    {project.tag}
+                    {this.putGitHub()}&nbsp;
+                    {this.putWebsite()}&nbsp;
+                    {this.putTag()}
                 </p>
             </div>
         );

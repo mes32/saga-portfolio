4058a86afaff28f3b1d7e6e39f353dad2b135f92
mes32 <stockman.mike@gmail.com>
2019-02-01 23:02:53 -0600

    Add some input fields to AdminPage.

--- src/components/AdminInputForm/AdminInputForm.js
+++ src/components/AdminInputForm/AdminInputForm.js
@@ -5,29 +5,52 @@ class AdminPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            projectName: '',
-            dateFinished: '',
+            name: '',
+            date: '',
             tag: '',
             github: '',
             website: '',
-            desciption: ''
+            description: ''
         };
     }
 
     changeName = (event) => {
-        // this.setState({
-        //     ...this.state,
-        //     event.target.value,
-        // });
-        console.log(event.target.value);
+        this.setState({
+            ...this.state,
+            name: event.target.value,
+        });
+    }
+
+    changeGitHub = (event) => {
+        this.setState({
+            ...this.state,
+            github: event.target.value,
+        });
+    }
+
+    changeWebsite = (event) => {
+        this.setState({
+            ...this.state,
+            website: event.target.value,
+        });
+    }
+
+    submit = (event) => {
+        event.preventDefault();
+        console.log(this.state);
     }
 
     render() {
         return (
             <div>
                 <h2>Add New Project</h2>
-                <form>
+                <form onSubmit={this.submit}>
                     <input onChange={this.changeName} placeholder="Name" type="text" required />
+                    
+                    <input onChange={this.changeGitHub} placeholder="GitHub URL" type="text" />
+                    <input onChange={this.changeWebsite} placeholder="Website URL" type="text" />
+                    <input onChange={this.changeDescription} placeholder="Description" type="text" />
+                    <input type="submit" value="Submit" />
                 </form>
             </div>
         );

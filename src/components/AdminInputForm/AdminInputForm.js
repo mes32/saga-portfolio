fde6bfca27358da85fe791b5e3676b0e4e052b93
mes32 <stockman.mike@gmail.com>
2019-02-03 13:32:45 -0600

    Add form fields to AdminInputForm.

--- src/components/AdminInputForm/AdminInputForm.js
+++ src/components/AdminInputForm/AdminInputForm.js
@@ -12,8 +12,19 @@ class AdminPage extends Component {
             website: '',
             description: ''
         };
+
+        // TODO: Remove this initial state used for testing
+        this.state = {
+            name: 'RAAQ! Pizza Parlour',
+            date: '2019-01-25',
+            tag: 'React',
+            github: 'https://github.com/mes32/raaq-pizza-parlour',
+            website: '',
+            description: 'An example CRUD application that allows users to order pizza'
+        };
     }
 
+    // Handle changes to the 'Name' field
     changeName = (event) => {
         this.setState({
             ...this.state,
@@ -21,6 +32,26 @@ class AdminPage extends Component {
         });
     }
 
+    // TODO: Handle changes to all these text fields using a single onChange 
+    // function
+
+    // Handle changes to the 'Date' field
+    changeDate = (event) => {
+        this.setState({
+            ...this.state,
+            date: event.target.value,
+        });
+    }
+
+    // Handle changes to the 'Tag' field
+    changeTag = (event) => {
+        this.setState({
+            ...this.state,
+            tag: event.target.value,
+        });
+    }
+
+    // Handle changes to the 'GitHub' field
     changeGitHub = (event) => {
         this.setState({
             ...this.state,
@@ -28,6 +59,7 @@ class AdminPage extends Component {
         });
     }
 
+    // Handle changes to the 'Website' field
     changeWebsite = (event) => {
         this.setState({
             ...this.state,
@@ -35,21 +67,29 @@ class AdminPage extends Component {
         });
     }
 
+    // Handle presses to the 'Submit' button
     submit = (event) => {
         event.preventDefault();
         console.log(this.state);
     }
 
+    // Display this component on the webpage
     render() {
         return (
             <div>
                 <h2>Add New Project</h2>
                 <form onSubmit={this.submit}>
                     <input onChange={this.changeName} placeholder="Name" type="text" required />
-                    
+                    {/* TODO: Make Date and Tag more specific input types */}
+                    <input onChange={this.changeDate} placeholder="Date" type="text" />
+                    <input onChange={this.changeTag} placeholder="Tag" type="text" />
+                    {/* TODO: Format the form in a way that does not rely on <br /> */}
+                    <br />
                     <input onChange={this.changeGitHub} placeholder="GitHub URL" type="text" />
                     <input onChange={this.changeWebsite} placeholder="Website URL" type="text" />
                     <input onChange={this.changeDescription} placeholder="Description" type="text" />
+                    {/* TODO: Format the form in a way that does not rely on <br /> */}
+                    <br />
                     <input type="submit" value="Submit" />
                 </form>
             </div>
